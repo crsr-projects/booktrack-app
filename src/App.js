d7c76897232b096c4c6a1874c813e205974e58cc
= <=>
2025-09-20 14:41:07 +0000

    feat: Fetch and display list of books from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import BookList from './components/BookList';
+
 import './App.css';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>BookTrack</h1>
-    </div>
-  );
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/books')
+            .then(response => response.json())
+            .then(data => setBooks(data))
+            .catch(error => console.error("Error fetching data:", error));
+        }, []);
+        return (
+            <div className="App">
+                <BookList books={books} />
+            </div>
+          );     
+
 }
 export default App;
---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function BookList({ books }) {
+    return (
+        <div className="book-list">
+          {books.map(book => (
+            <div key={book.id}>
+              {book.title}
+            </div>
+          ))}
+        </div>
+      );
+}
+export default BookList;
